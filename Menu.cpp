54b57352f0e831a684baf681aedf94084db39445
MaxSlukovskyi <maksym.slukovskyi@gmail.com>
2020-06-11 13:01:10 +0300

    3.0

--- Menu.cpp
+++ Menu.cpp
@@ -2,17 +2,17 @@
 #include <string>
 #include <iostream>
 
-void MainMenu(int, char*);
-int SecondMenu1(int, int, char*);
+int MainMenu(int, int, char*);
+int SecondMenu(int, int);
 
 int main()
 {
 	initwindow(1920, 1080);
 	int number = -1, k = 0, numberVertical = -1;
 	char key = 'd';
-	MainMenu(number, &key);
+	MainMenu(number, -2, &key);
 	label1:
-	MainMenu(number, &key);
+	MainMenu(number, -2, &key);
 	label2:
 	switch(key)
 	{
@@ -25,7 +25,7 @@ int main()
 			if(number == 0)
 				goto label1;
 			number--;
-			MainMenu(number, &key);
+			MainMenu(number, -2, &key);
 			goto label2;
 		}
 		case 'd': //right
@@ -37,7 +37,7 @@ int main()
 			if(number == 4)
 				goto label1;
 			number++;
-			MainMenu(number, &key);
+			MainMenu(number, -2, &key);
 			goto label2;
 		}
 		case 's':
@@ -49,8 +49,8 @@ int main()
 				if(numberVertical == 1)
 					goto label1;
 				numberVertical++;
-				SecondMenu1(number, numberVertical, &key);
-				goto label1;
+				MainMenu(number, numberVertical, &key);
+				goto label2;
 			}
 		}
 		case 'w':
@@ -61,9 +61,12 @@ int main()
 			{
 				if(numberVertical == 0)
 					goto label1;
-				numberVertical--;
-				SecondMenu1(number, numberVertical, &key);
-				goto label2;
+				else
+				{
+					numberVertical--;
+					MainMenu(number, numberVertical, &key);
+					goto label2;
+				}
 			}
 		}
 		case 13:
@@ -72,8 +75,8 @@ int main()
 				return 0;
 			else
 			{
-				if(SecondMenu1(number, -1, &key) == 1)
-					k = 1;
+				k = MainMenu(number, -1, &key);
+				goto label2;
 			}
 		}	
 		default:
@@ -84,16 +87,17 @@ int main()
 	return 0;
 }
 
-void MainMenu(int number, char *key)
+int MainMenu(int number, int numberVertical, char *key)
 {
 	settextstyle(6, 0, 1);
-	int a = 0;
+	int a = 0, k;
 	if(number == 0)
 	{
 		setfillstyle(1, 10);
 		a = 10;
 		bar(0, 0, getmaxx() / 5 - 1, getmaxy() / 9 + a);
 		outtextxy(getmaxx() / 10 - 30, getmaxy() / 18 - 10 + a / 2, "Дія 1");
+		k = SecondMenu(number, numberVertical);
 		*key = getch();
 		setfillstyle(1, 0);	
 		bar(0, 0, getmaxx() / 5 - 1, getmaxy() / 9 + a);
@@ -108,6 +112,7 @@ void MainMenu(int number, char *key)
 		a = 10;	
 		bar(getmaxx() / 5 + 1, 0, getmaxx() * 2 / 5 - 1, getmaxy() / 9 + a);
 		outtextxy(getmaxx() / 5 + getmaxx() / 10 - 30, getmaxy() / 18 - 10 + a / 2, "Дія 2");
+		k = SecondMenu(number, numberVertical);
 		*key = getch();
 		setfillstyle(1, 0);
 		bar(getmaxx() / 5 + 1, 0, getmaxx() * 2 / 5 - 1, getmaxy() / 9 + a);
@@ -122,6 +127,7 @@ void MainMenu(int number, char *key)
 		a = 10;
 		bar(getmaxx() * 2 / 5 + 1, 0, getmaxx() * 3 / 5 - 1, getmaxy() / 9 + a);
 		outtextxy(getmaxx() * 2 / 5 + getmaxx() / 10 - 30, getmaxy() / 18 - 10 + a /2, "Дія 3");
+		k = SecondMenu(number, numberVertical);
 		*key = getch();
 		setfillstyle(1, 0);
 		bar(getmaxx() * 2 / 5 + 1, 0, getmaxx() * 3 / 5 - 1, getmaxy() / 9 + a);	
@@ -136,6 +142,7 @@ void MainMenu(int number, char *key)
 		a = 10;
 		bar(getmaxx() * 3 / 5 + 1 , 0, getmaxx() * 4 / 5 - 1, getmaxy() / 9 + a);
 		outtextxy(getmaxx() * 3 / 5 + getmaxx() / 10 - 30, getmaxy() / 18 - 10 + a / 2, "Дія 4");
+		k = SecondMenu(number, numberVertical);
 		*key = getch();
 		setfillstyle(1, 0);
 		bar(getmaxx() * 3 / 5 + 1 , 0, getmaxx() * 4 / 5 - 1, getmaxy() / 9 + a);
@@ -158,46 +165,41 @@ void MainMenu(int number, char *key)
 	setfillstyle(1, 9);
 	bar(getmaxx() * 4 / 5 + 1, 0, getmaxx(), getmaxy() / 9 + a);
 	outtextxy(getmaxx() * 4 / 5 + getmaxx() / 10 - 30, getmaxy() / 18 - 10 + a /2, "Вихід");
+	return k;
 }
 
-int SecondMenu1(int number, int numberVertical, char *key)
+int SecondMenu(int number, int numberVertical)
 {
+	int k;
 	settextstyle(6, 0, 1);
-	if (number == 4)
-		goto label4;
 	if(numberVertical == 0)
-	{
-		setfillstyle(1, 14);
-		bar(number * getmaxx() / 5, getmaxy() / 5, (getmaxx() / 5 - 1) * (number + 1), getmaxy() / 5 + (getmaxy() / 5 - 30 - getmaxy() / 9));
-		outtextxy(getmaxx() / 10 - 60 + getmaxx() / 5 * number, getmaxy() / 9 + 40 + (getmaxy() / 5 - 30 - getmaxy() / 9), "Побудова 2");
-		setfillstyle(1, 7);
-		bar(number * getmaxx() / 5, getmaxy() / 9 + 20, (getmaxx() / 5 - 1) * (number + 1), getmaxy() / 5 - 10);
-		outtextxy(getmaxx() / 10 - 60 + getmaxx() / 5 * number, getmaxy() / 9 + 10 + (getmaxy() / 5 - 30 - getmaxy() / 9) / 2, "Побудова 1");
-		*key = getch();
-	}
-	else
-	{
-		setfillstyle(1, 14);
-		bar(number * getmaxx() / 5, getmaxy() / 9 + 20, (getmaxx() / 5 - 1) * (number + 1), getmaxy() / 5 - 10);
-		outtextxy(getmaxx() / 10 - 60 + getmaxx() / 5 * number, getmaxy() / 9 + 10 + (getmaxy() / 5 - 30 - getmaxy() / 9) / 2, "Побудова 1");
-	}
-	setfillstyle(1, 14);
-	settextstyle(6, 0, 1);
-	if(numberVertical == 1)
-	{
-		setfillstyle(1, 7);
-		bar(number * getmaxx() / 5, getmaxy() / 5, (getmaxx() / 5 - 1) * (number + 1), getmaxy() / 5 + (getmaxy() / 5 - 30 - getmaxy() / 9));
-		outtextxy(getmaxx() / 10 - 60 + getmaxx() / 5 * number, getmaxy() / 9 + 40 + (getmaxy() / 5 - 30 - getmaxy() / 9), "Побудова 2");
-		*key = getch();
-	}
-	else
-	{
-		setfillstyle(1, 14);
-		bar(number * getmaxx() / 5, getmaxy() / 5, (getmaxx() / 5 - 1) * (number + 1), getmaxy() / 5 + (getmaxy() / 5 - 30 - getmaxy() / 9));
-		outtextxy(getmaxx() / 10 - 60 + getmaxx() / 5 * number, getmaxy() / 9 + 40 + (getmaxy() / 5 - 30 - getmaxy() / 9), "Побудова 2");
-	}
-	setfillstyle(1, 14);
-	return 1;
-	label4:;
-	return 0;
+		{
+			setfillstyle(1, 14);
+			bar(number * getmaxx() / 5, getmaxy() / 5, (getmaxx() / 5 - 1) * (number + 1), getmaxy() / 5 + (getmaxy() / 5 - 30 - getmaxy() / 9));
+			outtextxy(getmaxx() / 10 - 60 + getmaxx() / 5 * number, getmaxy() / 9 + 40 + (getmaxy() / 5 - 30 - getmaxy() / 9), "Побудова 2");
+			setfillstyle(1, 7);
+			bar(number * getmaxx() / 5, getmaxy() / 9 + 20, (getmaxx() / 5 - 1) * (number + 1), getmaxy() / 5 - 10);
+			outtextxy(getmaxx() / 10 - 60 + getmaxx() / 5 * number, getmaxy() / 9 + 10 + (getmaxy() / 5 - 30 - getmaxy() / 9) / 2, "Побудова 1");
+		}
+		else if(numberVertical == -1)
+		{
+			
+			setfillstyle(1, 14);
+			bar(number * getmaxx() / 5, getmaxy() / 9 + 20, (getmaxx() / 5 - 1) * (number + 1), getmaxy() / 5 - 10);
+			outtextxy(getmaxx() / 10 - 60 + getmaxx() / 5 * number, getmaxy() / 9 + 10 + (getmaxy() / 5 - 30 - getmaxy() / 9) / 2, "Побудова 1");
+			setfillstyle(1, 14);
+			bar(number * getmaxx() / 5, getmaxy() / 5, (getmaxx() / 5 - 1) * (number + 1), getmaxy() / 5 + (getmaxy() / 5 - 30 - getmaxy() / 9));
+			outtextxy(getmaxx() / 10 - 60 + getmaxx() / 5 * number, getmaxy() / 9 + 40 + (getmaxy() / 5 - 30 - getmaxy() / 9), "Побудова 2");
+			k = 1;
+		}
+		if(numberVertical == 1)
+		{
+			setfillstyle(1, 7);
+			bar(number * getmaxx() / 5, getmaxy() / 5, (getmaxx() / 5 - 1) * (number + 1), getmaxy() / 5 + (getmaxy() / 5 - 30 - getmaxy() / 9));
+			outtextxy(getmaxx() / 10 - 60 + getmaxx() / 5 * number, getmaxy() / 9 + 40 + (getmaxy() / 5 - 30 - getmaxy() / 9), "Побудова 2");
+			setfillstyle(1, 14);
+			bar(number * getmaxx() / 5, getmaxy() / 9 + 20, (getmaxx() / 5 - 1) * (number + 1), getmaxy() / 5 - 10);
+			outtextxy(getmaxx() / 10 - 60 + getmaxx() / 5 * number, getmaxy() / 9 + 10 + (getmaxy() / 5 - 30 - getmaxy() / 9) / 2, "Побудова 1");
+		}
+	return k;
 }
