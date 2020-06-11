3a77dc78331498d497335e6fd60f6bba5189b487
MaxSlukovskyi <maksym.slukovskyi@gmail.com>
2020-06-11 10:44:23 +0300

    2.0

--- Menu.cpp
+++ Menu.cpp
@@ -3,12 +3,12 @@
 #include <iostream>
 
 void MainMenu(int, char*);
-void SecondMenu1(int);
+int SecondMenu1(int, int, char*);
 
 int main()
 {
 	initwindow(1920, 1080);
-	int number = -1;
+	int number = -1, k = 0, numberVertical = -1;
 	char key = 'd';
 	MainMenu(number, &key);
 	label1:
@@ -18,8 +18,10 @@ int main()
 	{
 		case 'a': //left
 		{
+			numberVertical = -1;
 			setfillstyle(1, 0);
 			bar(0, getmaxy() / 9 + 20, getmaxx(), getmaxy() / 5 + (getmaxy() / 5 - 30 - getmaxy() / 9));
+			k = 0;
 			if(number == 0)
 				goto label1;
 			number--;
@@ -28,32 +30,51 @@ int main()
 		}
 		case 'd': //right
 		{
+			numberVertical = -1;
 			setfillstyle(1, 0);
 			bar(0, getmaxy() / 9 + 20, getmaxx(), getmaxy() / 5 + (getmaxy() / 5 - 30 - getmaxy() / 9));
+			k = 0;
 			if(number == 4)
 				goto label1;
 			number++;
 			MainMenu(number, &key);
 			goto label2;
 		}
-		/*case 's':
+		case 's':
 		{
-			number++;
-			MainMenu(number, &key);
-			goto label2;
+			if(k == 0)
+				goto label1;
+			else
+			{
+				if(numberVertical == 1)
+					goto label1;
+				numberVertical++;
+				SecondMenu1(number, numberVertical, &key);
+				goto label1;
+			}
 		}
 		case 'w':
 		{
-			number++;
-			MainMenu(number, &key);
-			goto label2;
-		}*/
+			if(k == 0)
+				goto label1;
+			else
+			{
+				if(numberVertical == 0)
+					goto label1;
+				numberVertical--;
+				SecondMenu1(number, numberVertical, &key);
+				goto label2;
+			}
+		}
 		case 13:
 		{
 			if(number == 4)
 				return 0;
 			else
-				SecondMenu1(number);
+			{
+				if(SecondMenu1(number, -1, &key) == 1)
+					k = 1;
+			}
 		}	
 		default:
 			goto label1;
@@ -139,15 +160,44 @@ void MainMenu(int number, char *key)
 	outtextxy(getmaxx() * 4 / 5 + getmaxx() / 10 - 30, getmaxy() / 18 - 10 + a /2, "Вихід");
 }
 
-void SecondMenu1(int number)
+int SecondMenu1(int number, int numberVertical, char *key)
 {
+	settextstyle(6, 0, 1);
 	if (number == 4)
 		goto label4;
+	if(numberVertical == 0)
+	{
+		setfillstyle(1, 14);
+		bar(number * getmaxx() / 5, getmaxy() / 5, (getmaxx() / 5 - 1) * (number + 1), getmaxy() / 5 + (getmaxy() / 5 - 30 - getmaxy() / 9));
+		outtextxy(getmaxx() / 10 - 60 + getmaxx() / 5 * number, getmaxy() / 9 + 40 + (getmaxy() / 5 - 30 - getmaxy() / 9), "Побудова 2");
+		setfillstyle(1, 7);
+		bar(number * getmaxx() / 5, getmaxy() / 9 + 20, (getmaxx() / 5 - 1) * (number + 1), getmaxy() / 5 - 10);
+		outtextxy(getmaxx() / 10 - 60 + getmaxx() / 5 * number, getmaxy() / 9 + 10 + (getmaxy() / 5 - 30 - getmaxy() / 9) / 2, "Побудова 1");
+		*key = getch();
+	}
+	else
+	{
+		setfillstyle(1, 14);
+		bar(number * getmaxx() / 5, getmaxy() / 9 + 20, (getmaxx() / 5 - 1) * (number + 1), getmaxy() / 5 - 10);
+		outtextxy(getmaxx() / 10 - 60 + getmaxx() / 5 * number, getmaxy() / 9 + 10 + (getmaxy() / 5 - 30 - getmaxy() / 9) / 2, "Побудова 1");
+	}
 	setfillstyle(1, 14);
-	bar(number * getmaxx() / 5, getmaxy() / 9 + 20, (getmaxx() / 5 - 1) * (number + 1), getmaxy() / 5 - 10);
 	settextstyle(6, 0, 1);
-	outtextxy(getmaxx() / 10 - 60 + getmaxx() / 5 * number, getmaxy() / 9 + 10 + (getmaxy() / 5 - 30 - getmaxy() / 9) / 2, "Побудова 1");
-	bar(number * getmaxx() / 5, getmaxy() / 5, (getmaxx() / 5 - 1) * (number + 1), getmaxy() / 5 + (getmaxy() / 5 - 30 - getmaxy() / 9));
-	outtextxy(getmaxx() / 10 - 60 + getmaxx() / 5 * number, getmaxy() / 9 + 40 + (getmaxy() / 5 - 30 - getmaxy() / 9), "Побудова 2");
+	if(numberVertical == 1)
+	{
+		setfillstyle(1, 7);
+		bar(number * getmaxx() / 5, getmaxy() / 5, (getmaxx() / 5 - 1) * (number + 1), getmaxy() / 5 + (getmaxy() / 5 - 30 - getmaxy() / 9));
+		outtextxy(getmaxx() / 10 - 60 + getmaxx() / 5 * number, getmaxy() / 9 + 40 + (getmaxy() / 5 - 30 - getmaxy() / 9), "Побудова 2");
+		*key = getch();
+	}
+	else
+	{
+		setfillstyle(1, 14);
+		bar(number * getmaxx() / 5, getmaxy() / 5, (getmaxx() / 5 - 1) * (number + 1), getmaxy() / 5 + (getmaxy() / 5 - 30 - getmaxy() / 9));
+		outtextxy(getmaxx() / 10 - 60 + getmaxx() / 5 * number, getmaxy() / 9 + 40 + (getmaxy() / 5 - 30 - getmaxy() / 9), "Побудова 2");
+	}
+	setfillstyle(1, 14);
+	return 1;
 	label4:;
+	return 0;
 }
